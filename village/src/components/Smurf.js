44e2c872cddc52411f7b85554de235f928213782
Ken Ruf <ruf.kenichi@gmail.com>
2019-06-16 21:59:28 -0500

    fixed bug where clicking on delete/update buttons also redirected to the specific smurf's link.

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -3,13 +3,15 @@ import {Link} from 'react-router-dom';
 
 const Smurf = props => {
   return (
+    <div>
     <Link to={`/smurf/${props.smurf.id}`} onClick={() => props.selectActiveSmurf(props.smurf)}>
       <h3>{props.smurf.name}</h3>
       <strong>{props.smurf.height} tall</strong>
       <p>{props.smurf.age} smurf years old</p>
-      <button onClick={() => props.deleteSmurf(props.smurf)}>Delete Smurf</button>
-      <button onClick={() => props.selectActiveSmurf(props.smurf)}>Update Smurf</button>
     </Link>
+    <button onClick={() => props.deleteSmurf(props.smurf)}>Delete Smurf</button>
+    <button onClick={() => props.selectActiveSmurf(props.smurf)}>Update Smurf</button>
+    </div>
   );
 };
 
