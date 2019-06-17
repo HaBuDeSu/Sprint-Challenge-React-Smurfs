1b8ec90261721f81d7853354cad0f671b31767b0
Ken Ruf <ruf.kenichi@gmail.com>
2019-06-16 22:18:39 -0500

    styled smurf list

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom';
 
 const Smurf = props => {
   return (
-    <div>
-    <Link to={`/smurf/${props.smurf.id}`} onClick={() => props.selectActiveSmurf(props.smurf)}>
-      <h3>{props.smurf.name}</h3>
-      <strong>{props.smurf.height} tall</strong>
-      <p>{props.smurf.age} smurf years old</p>
-    </Link>
-    <button onClick={() => props.deleteSmurf(props.smurf)}>Delete Smurf</button>
-    <button onClick={() => props.selectActiveSmurf(props.smurf)}>Update Smurf</button>
+    <div className="smurf">
+      <Link to={`/smurf/${props.smurf.id}`} onClick={() => props.selectActiveSmurf(props.smurf)}>
+        <h3>{props.smurf.name}</h3>
+        <strong>{props.smurf.height} tall</strong>
+        <p>{props.smurf.age} smurf years old</p>
+      </Link>
+    <button className="delete-button" onClick={() => props.deleteSmurf(props.smurf)}>Delete Smurf</button>
+    <button className="update-button" onClick={() => props.selectActiveSmurf(props.smurf)}>Update Smurf</button>
     </div>
   );
 };
